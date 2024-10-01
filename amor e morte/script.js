bf09a1961d8b684db2f304f02ef58ac8349c14ce
IVDAJC <00001077146486sp@al.educacao.sp.gov.br>
2024-10-01 14:42:53 -0300

    Add files via upload

---
+++ amor e morte/script.js
@@ -0,0 +1,8 @@
+const avanca = document.querySelectorAll ('.btn-proximo')
+        avanca.fortEach (button => {
+button.addEventlistener ('click' , function 
+const atual = document.queryselector ('ativo');
+const proximoPasso = 'passo-'+ this.getAttribute ('data-proximo');
+       }atual.class.remove ('ativo');
+    document.getElementById(proxiPasso)
+})
